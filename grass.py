735cb7e40e4a67e32eb66674c5b698cd7d6f9cdf
orientsvictor <sourjyas.work@gmail.com>
2024-12-21 16:54:14 +0530

    implemented merton in grass

--- grass.py
+++ grass.py
@@ -45,7 +45,7 @@ def df_merton(path):
     # Drop NaN values caused by shift
     data = data.dropna(subset=["GROWTH RATE"])
 
-    return df_merton
+    return data
 
 def merton_params(df):
     volatility = df["GROWTH RATE"].std()
@@ -58,38 +58,58 @@ def pd_merton(volatility, growth_rate, tenure, net_worth, debt_point):
     d2 = (np.log(net_worth / debt_point) + (growth_rate - (volatility**2) / 2) * tenure) / (volatility * np.sqrt(tenure))
     return norm.cdf(-d2)
 
+def risk_premium(pd, lgd):
+    return pd*lgd
+
+def credit_score(pd, lgd, ead):
+    beta = (300 - 850)/ead
+    ecl = pd * lgd * ead
+    return 850 + (beta * ecl)
+
+def annual_interest_rate(risk_premium, profit_rate):
+    return risk_premium + profit_rate
+
 if __name__ == '__main__':
-    age = int(input("Enter borrower age: "))
-    gender = input("Enter gender (F/M/TG): ")
-    asset_val = float(input("Enter net worth: "))
-    annual_income = float(input("Enter annual income: "))
-    monthly_expenses = float(input("Enter monthly expenses: "))
-    old_dependents = int(input("Number of old people dependent on borrower: "))
-    young_dependents = int(input("Number of young people dependent on borrower: "))
-    occupants_count = int(input("Number of occupants in household: "))
-    house_area = float(input("Enter house area (sq ft): "))
-    loan_amount = float(input("Enter loan amount: "))
-    loan_tenure = int(input("Enter number loan term (months): "))
+    # age = int(input("Enter borrower age: "))
+    # gender = input("Enter gender (F/M/TG): ")
+    # asset_val = float(input("Enter net worth: "))
+    # annual_income = float(input("Enter annual income: "))
+    # monthly_expenses = float(input("Enter monthly expenses: "))
+    # old_dependents = int(input("Number of old people dependent on borrower: "))
+    # young_dependents = int(input("Number of young people dependent on borrower: "))
+    # occupants_count = int(input("Number of occupants in household: "))
+    # house_area = float(input("Enter house area (sq ft): "))
+    # loan_amount = float(input("Enter loan amount: "))
+    # loan_tenure = int(input("Enter number loan term (months): "))
 
-    features = pd.DataFrame({
-        "age": [age],
-        "sex": [gender],
-        "annual_income": [annual_income],
-        "monthly_expenses": [monthly_expenses],
-        "old_dependents": [old_dependents],
-        "young_dependents": [young_dependents],
-        "occupants_count": [occupants_count],
-        "house_area": [house_area],
-        "loan_tenure": [loan_tenure],
-        "loan_installments": [loan_tenure],
-        "loan_amount": [loan_amount],
-        "LTI": [lti(loan_amount, annual_income, loan_tenure)],
-        "LSR": [lsr(loan_amount, loan_tenure, monthly_expenses)],
-        "specific_net_income": [specific_net_income(annual_income, monthly_expenses, loan_tenure)]
-    })
+    # features = pd.DataFrame({
+    #     "age": [age],
+    #     "sex": [gender],
+    #     "annual_income": [annual_income],
+    #     "monthly_expenses": [monthly_expenses],
+    #     "old_dependents": [old_dependents],
+    #     "young_dependents": [young_dependents],
+    #     "occupants_count": [occupants_count],
+    #     "house_area": [house_area],
+    #     "loan_tenure": [loan_tenure],
+    #     "loan_installments": [loan_tenure],
+    #     "loan_amount": [loan_amount],
+    #     "LTI": [lti(loan_amount, annual_income, loan_tenure)],
+    #     "LSR": [lsr(loan_amount, loan_tenure, monthly_expenses)],
+    #     "specific_net_income": [specific_net_income(annual_income, monthly_expenses, loan_tenure)]
+    # })
 
-    model = load_model('./models/rf_lgd_pipeline.pkl')
-    y_pred = lgd(features, model)
-    print("LGD: ", y_pred)
+    # model = load_model('./models/rf_lgd_pipeline.pkl')
+    # y_pred = lgd(features, model)
+    # print("LGD: ", y_pred)
 
+    lgd = 0.2
+    ead = 100000
+    df = df_merton("./datasets/bank.csv")
+    volatility, growth_rate = merton_params(df)
+    pd = pd_merton(volatility, growth_rate, 1, 10000, 800)
+    rp = risk_premium(pd, lgd)
+    cs = credit_score(pd, lgd, ead)
+    ir = annual_interest_rate(rp, 0.03)
 
+    print(volatility, growth_rate, pd, rp, cs, ir)
